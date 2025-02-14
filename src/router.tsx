fa89796f7326dc51d97c383113c5af24f6e8d062
miaslls <dev@camilasalles.dev>
2025-02-14 13:40:29 -0300

    feat: router

---
+++ src/router.tsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  { path: '/', element: <>{'path: /'}</> },
+  { path: '/EXC365GD1', element: <>{'path: /EXC365GD1'}</> },
+  { path: '/EXC365GD2', element: <>{'path: /EXC365GD2'}</> },
+]);
+
+export default router;
