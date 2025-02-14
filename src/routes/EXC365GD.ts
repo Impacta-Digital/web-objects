4383e68245ee07e89e048fa108a6b75b0125d327
miaslls <dev@camilasalles.dev>
2025-02-14 14:12:03 -0300

    feat: EXC365GD routes

---
+++ src/routes/EXC365GD.ts
@@ -0,0 +1,16 @@
+export const baseUrl = 'https://web-objects-2p54r.ondigitalocean.app/' as const;
+
+export const path = {
+  basic: {
+    1: '5a8506a7-7e42-490d-ab3b-729068d7ca74',
+    2: 'e7145ae7-f875-4749-bb8c-bef03cc1479c',
+    3: '5ea45007-6bf6-4ebf-bfbf-1327792d9355',
+    4: 'e3604623-b310-494d-bf0f-d159f3ce35c0',
+  },
+  advanced: {
+    1: '27d3e264-2585-4ca7-9fa8-aece8ff97b06',
+    2: '1185cd0d-bcbd-419e-a1d9-7ea81013cc8e',
+    3: 'b6b55614-a64d-493b-b276-9273430ec391',
+    4: '6b44859b-fe76-4b82-bcb8-a6d6215187f0',
+  },
+} as const;
