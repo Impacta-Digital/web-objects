03955fe821724059b0ede60cd0048efdb696dce7
miaslls <dev@camilasalles.dev>
2025-02-17 14:15:35 -0300

    chore: include imageData files

---
+++ src/pages/EXC365GD1/assets/imageData.ts
@@ -0,0 +1,34 @@
+import lab02_1 from './lab02_1.jpg';
+import lab02_2 from './lab02_2.jpg';
+import lab02_3 from './lab02_3.jpg';
+import lab02_4 from './lab02_4.jpg';
+import lab02_5 from './lab02_5.jpg';
+import img03_1 from './img03_1.jpg';
+import img03_2 from './img03_2.jpg';
+import lab03_1 from './lab03_1.jpg';
+import lab03_2 from './lab03_2.jpg';
+import lab03_3 from './lab03_3.jpg';
+import lab03_4 from './lab03_4.jpg';
+import lab03_5 from './lab03_5.jpg';
+import lab03_6 from './lab03_6.jpg';
+import lab03_7 from './lab03_7.jpg';
+import lab03_8 from './lab03_8.jpg';
+import img04_1 from './img04_1.jpg';
+import img04_2 from './img04_2.jpg';
+import img04_3 from './img04_3.jpg';
+import img04_4 from './img04_4.jpg';
+import img04_5 from './img04_5.jpg';
+import img04_6 from './img04_6.jpg';
+import img04_7 from './img04_7.jpg';
+import lab04_1 from './lab04_1.jpg';
+import lab04_2 from './lab04_2.jpg';
+import lab04_3 from './lab04_3.jpg';
+import lab04_4 from './lab04_4.jpg';
+
+const imageData = {
+  LabBasic02: [lab02_1, lab02_2, lab02_3, lab02_4, lab02_5],
+  LabBasic03: [img03_1, img03_2, lab03_1, lab03_2, lab03_3, lab03_4, lab03_5, lab03_6, lab03_7, lab03_8],
+  LabBasic04: [img04_1, img04_2, img04_3, img04_4, img04_5, img04_6, img04_7, lab04_1, lab04_2, lab04_3, lab04_4],
+};
+
+export default imageData;
---
+++ src/pages/EXC365GD2/assets/imageData.ts
@@ -0,0 +1,91 @@
+import lab01_1 from './lab01_1.jpg';
+import lab01_2 from './lab01_2.jpg';
+import lab01_3 from './lab01_3.jpg';
+import lab01_4 from './lab01_4.jpg';
+import lab01_5 from './lab01_5.jpg';
+import lab01_6 from './lab01_6.jpg';
+import img02_1 from './img02_1.jpg';
+import img02_2 from './img02_2.jpg';
+import lab02_1 from './lab02_1.jpg';
+import lab02_2 from './lab02_2.jpg';
+import lab02_3 from './lab02_3.jpg';
+import lab03_1 from './lab03_1.png';
+import lab03_2 from './lab03_2.png';
+import lab03_3 from './lab03_3.jpg';
+import lab03_4 from './lab03_4.png';
+import lab03_5 from './lab03_5.png';
+import lab03_6 from './lab03_6.jpg';
+import lab04_1 from './lab04_1.jpg';
+import lab04_2 from './lab04_2.jpg';
+import lab04_3 from './lab04_3.jpg';
+import lab04_4 from './lab04_4.png';
+import lab04_5 from './lab04_5.png';
+import lab04_6 from './lab04_6.png';
+import lab04_7 from './lab04_7.png';
+import lab04_8 from './lab04_8.png';
+import lab04_9 from './lab04_9.png';
+import lab04_10 from './lab04_10.png';
+import lab04_11 from './lab04_11.png';
+import lab04_12 from './lab04_12.png';
+import lab04_13 from './lab04_13.png';
+import lab04_14 from './lab04_14.png';
+import lab04_15 from './lab04_15.png';
+import lab04_16 from './lab04_16.png';
+import lab04_17 from './lab04_17.png';
+import lab04_18 from './lab04_18.png';
+import lab04_19 from './lab04_19.png';
+import lab04_20 from './lab04_20.png';
+import lab04_21 from './lab04_21.png';
+import lab04_22 from './lab04_22.png';
+import lab04_23 from './lab04_23.png';
+import lab04_24 from './lab04_24.png';
+import lab04_25 from './lab04_25.png';
+import lab04_26 from './lab04_26.jpg';
+import lab04_27 from './lab04_27.png';
+import lab04_28 from './lab04_28.png';
+import lab04_29 from './lab04_29.png';
+import lab04_30 from './lab04_30.png';
+import lab04_31 from './lab04_31.png';
+import lab04_32 from './lab04_32.jpg';
+
+const imageData = {
+  LabAdvanced01: [lab01_1, lab01_2, lab01_3, lab01_4, lab01_5, lab01_6],
+  LabAdvanced02: [img02_1, img02_2, lab02_1, lab02_2, lab02_3],
+  LabAdvanced03: [lab03_1, lab03_2, lab03_3, lab03_4, lab03_5, lab03_6],
+  LabAdvanced04: [
+    lab04_1,
+    lab04_2,
+    lab04_3,
+    lab04_4,
+    lab04_5,
+    lab04_6,
+    lab04_7,
+    lab04_8,
+    lab04_9,
+    lab04_10,
+    lab04_11,
+    lab04_12,
+    lab04_13,
+    lab04_14,
+    lab04_15,
+    lab04_16,
+    lab04_17,
+    lab04_18,
+    lab04_19,
+    lab04_20,
+    lab04_21,
+    lab04_22,
+    lab04_23,
+    lab04_24,
+    lab04_25,
+    lab04_26,
+    lab04_27,
+    lab04_28,
+    lab04_29,
+    lab04_30,
+    lab04_31,
+    lab04_32,
+  ],
+};
+
+export default imageData;
