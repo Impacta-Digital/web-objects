294501a8df1fe273daca06fd46cfba5425b145e5
miaslls <dev@camilasalles.dev>
2025-02-20 13:52:37 -0300

    chore(data): course & donwload

---
+++ src/data/EXC365GD/courseData.ts
@@ -0,0 +1,8 @@
+const courseTitle = 'Excel 365 – Gráficos e Dashboards';
+
+const courseData = {
+  EXC365GD1: `${courseTitle} | Básico`,
+  EXC365GD2: `${courseTitle} | Avançado`,
+};
+
+export default courseData;
---
+++ src/data/EXC365GD/downloadData.ts
@@ -0,0 +1,15 @@
+const downloadData = {
+  EXC365GD1: {
+    LabBasic02: '/downloads/excel365-graficos-dashboards-basico_lab-aula2.zip',
+    LabBasic03: '/downloads/excel365-graficos-dashboards-basico_lab-aula3.zip',
+    LabBasic04: '/downloads/excel365-graficos-dashboards-basico_lab-aula4.zip',
+  },
+  EXC365GD2: {
+    LabAdvanced01: '/downloads/excel365-graficos-dashboards-avancado_lab-aula1.zip',
+    LabAdvanced02: '/downloads/excel365-graficos-dashboards-avancado_lab-aula2.zip',
+    LabAdvanced03: '/downloads/excel365-graficos-dashboards-avancado_lab-aula3.zip',
+    LabAdvanced04: '/downloads/excel365-graficos-dashboards-avancado_lab-aula4.zip',
+  },
+};
+
+export default downloadData;
