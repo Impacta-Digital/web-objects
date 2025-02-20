bbe637f67819ad5f2e1483e9058b8cc5697ef6aa
miaslls <dev@camilasalles.dev>
2025-02-20 13:52:07 -0300

    chore: move

---
+++ src/data/EXC365GD/videoData.ts
@@ -0,0 +1,13 @@
+const videoData = {
+  EXC365GD1: {
+    LabBasic02: ['gAqjSGkUzFY'],
+    LabBasic03: ['OCFDyi4yyuU', 'WbODQ8tQSag'],
+    LabBasic04: ['75sc-K4nrrI', '7nQO7ntrA3A'],
+  },
+  EXC365GD2: {
+    LabAdvanced01: ['oxcl0Dx-n10', 'obzgtOqVR9U'],
+    LabAdvanced02: ['Gq7o3ibU4Qk'],
+  },
+};
+
+export default videoData;
