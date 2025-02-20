c480312601bcf2594d3c916d34f38aafdd5972b1
miaslls <dev@camilasalles.dev>
2025-02-20 10:44:13 -0300

    refactor: videoData

--- src/videoData.ts
+++ src/videoData.ts
@@ -1,12 +1,12 @@
 const videoData = {
   EXC365GD1: {
-    LabBasic02: ['gAqjSGkUzFY'],
-    LabBasic03: ['OCFDyi4yyuU', 'WbODQ8tQSag'],
-    LabBasic04: ['75sc-K4nrrI', '7nQO7ntrA3A'],
+    LabBasic02: { lab1: 'gAqjSGkUzFY' },
+    LabBasic03: { lab1: 'OCFDyi4yyuU', lab2: 'WbODQ8tQSag' },
+    LabBasic04: { lab1: '75sc-K4nrrI', lab2: '7nQO7ntrA3A' },
   },
   EXC365GD2: {
-    LabAdvanced01: ['oxcl0Dx-n10', 'obzgtOqVR9U'],
-    LabAdvanced02: ['Gq7o3ibU4Qk'],
+    LabAdvanced01: { lab1: 'oxcl0Dx-n10', lab2: 'obzgtOqVR9U' },
+    LabAdvanced02: { lab1: 'Gq7o3ibU4Qk' },
   },
 };
 
