a1bfacea347affdde0c9034a421506bd9d2ce2f7
miaslls <dev@camilasalles.dev>
2025-02-21 19:32:00 -0300

    chore: rename & minor styles

--- src/components/page-blocks/LabTitleAndDownload.tsx
+++ src/components/page-blocks/LabTitleAndDownload.tsx
@@ -1,12 +1,12 @@
 import { TitleAndDivider } from '../layout/TitleAndDivider';
 import { DownloadButton } from '../layout/DownloadButton';
 
-export interface LabTitleAndDownloadsProps {
+export interface LabTitleAndDownloadProps {
   classTitle: string;
   downloadPath: string;
 }
 
-export const LabTitleAndDownloads = (props: LabTitleAndDownloadsProps) => {
+export const LabTitleAndDownload = (props: LabTitleAndDownloadProps) => {
   const { classTitle, downloadPath } = props;
 
   return (
@@ -17,9 +17,10 @@ export const LabTitleAndDownloads = (props: LabTitleAndDownloadsProps) => {
 
       <TitleAndDivider title='Mãos à Obra!' subheading='Laboratórios' />
       <p>
-        Estes exercícios complementam o aprendizado. Faça-os usando os conceitos aprendidos na aula {classTitle}. Cada
-        exercício contém as instruções necessárias para você completar as atividades, bem como menção aos arquivos que
-        devem ser abertos, caso necessário. Baixe o material de apoio clicando no botão a seguir:
+        Estes exercícios complementam o aprendizado. Faça-os usando os conceitos aprendidos na aula{' '}
+        <strong>{classTitle}</strong>. Cada exercício contém as instruções necessárias para você completar as
+        atividades, bem como menção aos arquivos que devem ser abertos, caso necessário. Baixe o material de apoio
+        clicando no botão a seguir:
       </p>
 
       <DownloadButton filePath={downloadPath} buttonText='Material de Apoio' />
