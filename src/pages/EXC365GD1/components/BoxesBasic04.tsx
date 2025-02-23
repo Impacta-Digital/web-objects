880763c15a191bf5855876f000315067dcaa52e7
miaslls <dev@camilasalles.dev>
2025-02-23 09:29:12 -0300

    refactor(styles): substitute flex for grid

--- src/pages/EXC365GD1/components/BoxesBasic04.tsx
+++ src/pages/EXC365GD1/components/BoxesBasic04.tsx
@@ -5,26 +5,26 @@ import { ColorBox } from '../../../components/content/ColorBox';
 export const BoxesBasic04 = () => {
   const boxColors = [colors.purple, colors.cyan];
   const paragraphs = [
-    <>
+    <p className='--bulleted'>
       Há situações no dashboard em que um gráfico ocuparia muito espaço. Nesses casos, você poderá criar gráficos dentro
-      de células: <strong>minigráficos</strong>,{' '}
+      de células: <strong>mini-gráficos</strong>,{' '}
       <strong>formatação condicional com barras de dados ou escalas de cores</strong>, e{' '}
       <strong>gráficos usando repetição de caracteres</strong>;
-    </>,
-    <>
+    </p>,
+    <p className='--bulleted'>
       Quando uma série de dados representa valores muito pequenos e outra série valores muito grandes, dificilmente os
       valores pequenos poderão ser vistos no gráfico. Nesse caso, a melhor abordagem é fazer com que essa série exiba
       seus valores em outro eixo vertical, chamado <strong>eixo secundário</strong>. Dessa maneira, é possível
       acompanhar duas ordens de grandeza diferentes sobre o mesmo eixo horizontal. É possível, também, que as duas
       séries sejam representadas com tipos diferentes de gráficos. Esses são os gráficos de combinação e, geralmente,
       misturam gráficos de coluna e linha ou gráficos de coluna e área;
-    </>,
+    </p>,
   ];
 
   return (
     <div className={styles['container']}>
       {boxColors.map((color, index) => (
-        <ColorBox color={color} paragraph={paragraphs[index]} />
+        <ColorBox color={color} content={paragraphs[index]} />
       ))}
     </div>
   );
--- src/pages/EXC365GD2/components/BoxesAdvanced01.tsx
+++ src/pages/EXC365GD2/components/BoxesAdvanced01.tsx
@@ -5,18 +5,31 @@ import { ColorBox } from '../../../components/content/ColorBox';
 export const BoxesAdvanced01 = () => {
   const boxColors = [colors.purple, colors.cyan, colors.pink];
   const paragraphs = [
-    'Séries de dados que produzem gráficos de colunas verticais ou barras horizontais normalmente partem do valor 0. Se você desejar começar de um valor mínimo, será necessário representar uma primeira série contendo apenas o valor mínimo e uma segunda série contendo a diferença Máximo – Mínimo. Em seguida, a série Mínimo ficará sem preenchimento e sem contorno. A única função dela é dar “suporte” à serie que representa a diferença;',
-    'Outra técnica interessante é a que permite que você controle quais séries deseja exibir no gráfico, o que pode ser feito por meio de caixas de seleção, as quais atuam como “interruptores”. Dessa maneira, você pode comparar produtos de dois em dois, ou visualizar uma região por vez. Tudo isso sem precisar criar tabelas dinâmicas, aproveitando controles de formulário já construídos no dashboard.',
-    'O destaque de valores na planilha pode ser feito facilmente com formatação normal ou condicional. Porém, para destacar um ou mais pontos de dados em um gráfico, baseando-se em uma condição, será necessário usar alguns truques para você atingir o efeito desejado. Normalmente, envolvem o uso de séries que ficarão ocultas ou sobreposição de gráficos;',
+    <p className='--bulleted'>
+      Séries de dados que produzem gráficos de colunas verticais ou barras horizontais normalmente partem do valor 0. Se
+      você desejar começar de um valor mínimo, será necessário representar uma primeira série contendo apenas o valor
+      mínimo e uma segunda série contendo a diferença Máximo – Mínimo. Em seguida, a série Mínimo ficará sem
+      preenchimento e sem contorno. A única função dela é dar “suporte” à serie que representa a diferença;
+    </p>,
+    <p className='--bulleted'>
+      O destaque de valores na planilha pode ser feito facilmente com formatação normal ou condicional. Porém, para
+      destacar um ou mais pontos de dados em um gráfico, baseando-se em uma condição, será necessário usar alguns
+      truques para você atingir o efeito desejado. Normalmente, envolvem o uso de séries que ficarão ocultas ou
+      sobreposição de gráficos;
+    </p>,
+    <p className='--bulleted'>
+      Outra técnica interessante é a que permite que você controle quais séries deseja exibir no gráfico, o que pode ser
+      feito por meio de caixas de seleção, as quais atuam como “interruptores”. Dessa maneira, você pode comparar
+      produtos de dois em dois, ou visualizar uma região por vez. Tudo isso sem precisar criar tabelas dinâmicas,
+      aproveitando controles de formulário já construídos no dashboard.
+    </p>,
   ];
 
   return (
-    <div className={styles['outer-container']}>
-      <div className={styles['inner-container']}>
-        <ColorBox color={boxColors[0]} paragraph={paragraphs[0]} />
-        <ColorBox color={boxColors[1]} paragraph={paragraphs[1]} />
-      </div>
-      <ColorBox color={boxColors[2]} paragraph={paragraphs[2]} />
+    <div className={styles['boxes-grid']}>
+      <ColorBox color={boxColors[0]} content={paragraphs[0]} />
+      <ColorBox color={boxColors[1]} content={paragraphs[1]} />
+      <ColorBox color={boxColors[2]} content={paragraphs[2]} />
     </div>
   );
 };
