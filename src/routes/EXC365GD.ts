b5a2a660e26e8f0481418f888b48c70f4bfcf0af
miaslls <dev@camilasalles.dev>
2025-03-02 14:24:18 -0300

    fix: include path

--- src/routes/EXC365GD.ts
+++ src/routes/EXC365GD.ts
@@ -2,11 +2,13 @@ export const baseUrl = 'https://web-objects-2p54r.ondigitalocean.app/';
 
 export const pageId = {
   basic: {
+    path: 'EXC365GD1/',
     2: 'e7145ae7-f875-4749-bb8c-bef03cc1479c',
     3: '5ea45007-6bf6-4ebf-bfbf-1327792d9355',
     4: 'e3604623-b310-494d-bf0f-d159f3ce35c0',
   },
   advanced: {
+    path: 'EXC365GD2/',
     1: '27d3e264-2585-4ca7-9fa8-aece8ff97b06',
     2: '1185cd0d-bcbd-419e-a1d9-7ea81013cc8e',
     3: 'b6b55614-a64d-493b-b276-9273430ec391',
