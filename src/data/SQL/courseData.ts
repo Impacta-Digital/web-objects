e929c346dc3689b60e409800b84307c247d6e14a
miaslls <dev@camilasalles.dev>
2025-03-02 20:00:32 -0300

    fix: export

---
+++ src/data/SQL/courseData.ts
@@ -0,0 +1,22 @@
+export const courseTitle = 'SQL - Criando e Manipulando Banco de Dados';
+
+export const classTitles = {
+  intro: 'Introdução ao SQL Server',
+  1: 'Modelagem de dados',
+  2: 'Banco de dados',
+  3: 'Criar Tabelas',
+  4: 'Dicionário de Dados',
+  5: 'Instrução Insert',
+  6: 'Consultando Dados',
+  7: 'Expressões',
+  8: 'Associação de Tabelas',
+  9: 'Agrupamento de Dados',
+  10: 'Subquery ou Subconculta',
+  11: 'Instrução Update - Atualizar Dados',
+  12: 'Instrução Delete - Excluir Dados',
+  13: 'Tipos de Dados Definidos Pelo Usuário',
+  14: 'Operadores de Conjuntos e Instruções',
+  15: 'Introdução a Programação',
+  16: 'Introdução à Procedure',
+  17: 'Conteúdo Extra', // FIXME: positioning
+};
