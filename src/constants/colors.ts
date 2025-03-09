1179106902f08613a950e2b020cb35a9e0f3d991
miaslls <dev@camilasalles.dev>
2025-03-09 11:45:01 -0300

    feat(styles): new default styles

---
+++ src/constants/colors.ts
@@ -0,0 +1,49 @@
+export const colors = {
+  amberYellow: '#D4B400',
+  azureBlue: '#348AE4',
+  brickRed: '#CC3D25',
+  burntOrange: '#D87A15',
+  cloudWhite: '#F5F5F5',
+  deepAzure: '#2B6CB0',
+  emeraldGreen: '#28A745',
+  forestTeal: '#1F7D74',
+  graphiteGray: '#2D2D2D',
+  midnightBlack: '#1A1A1A',
+  pineGreen: '#1E7A34',
+  polarWhite: '#F8F9FA',
+  sageTeal: '#96B1AC',
+  slateMoss: '#546360',
+  turquoise: '#40E0D0',
+};
+
+export const darkTheme = {
+  background: colors.midnightBlack,
+  foreground: colors.cloudWhite,
+  secondaryText: colors.sageTeal,
+  accents: {
+    primary: colors.azureBlue,
+    secondary: colors.turquoise,
+  },
+  alerts: {
+    fail: colors.brickRed,
+    warning: colors.burntOrange,
+    info: colors.amberYellow,
+    success: colors.emeraldGreen,
+  },
+};
+
+export const lightTheme = {
+  background: colors.polarWhite,
+  foreground: colors.graphiteGray,
+  secondaryText: colors.slateMoss,
+  accents: {
+    primary: colors.deepAzure,
+    secondary: colors.forestTeal,
+  },
+  alerts: {
+    fail: colors.brickRed,
+    warning: colors.burntOrange,
+    info: colors.amberYellow,
+    success: colors.pineGreen,
+  },
+};
