c483b386f480a7343ed8e288e4ebc73b8f83fd24
miaslls <dev@camilasalles.dev>
2025-03-11 16:05:56 -0300

    feat: add label to `ImageContainer`

--- src/components/layout/ImageContainer.tsx
+++ src/components/layout/ImageContainer.tsx
@@ -7,6 +7,7 @@ export const ImageContainer = ({ src, alt }: ImageContainerProps) => {
   return (
     <div className='imageContainer'>
       <img src={src} alt={alt} />
+      <div className='imageLabel'>{alt}</div>
     </div>
   );
 };
