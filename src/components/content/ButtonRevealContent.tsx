62161bf457f284466f23a7dc0f05b9a535559de4
miaslls <dev@camilasalles.dev>
2025-03-16 07:37:36 -0300

    feat: `alignLeft` prop

--- src/components/content/ButtonRevealContent.tsx
+++ src/components/content/ButtonRevealContent.tsx
@@ -8,14 +8,15 @@ import { IconChevronRight } from '../icons/ChevronRight';
 export interface ButtonRevealContentProps {
   buttonText: string;
   content: React.ReactNode;
+  alignLeft?: boolean;
 }
 
-export const ButtonRevealContent = ({ buttonText, content }: ButtonRevealContentProps) => {
+export const ButtonRevealContent = ({ buttonText, content, alignLeft }: ButtonRevealContentProps) => {
   const [showContent, setShowContent] = useState<boolean>(false);
 
   return (
     <div className={styles.outerContainer}>
-      <div className='largeButtonContainer'>
+      <div className='largeButtonContainer' style={{ justifyContent: alignLeft ? 'flex-start' : 'center' }}>
         <div className='button largeButton' onClick={() => setShowContent((prev) => !prev)}>
           <div className='largeButtonIcon'>{showContent ? <IconChevronDown /> : <IconChevronRight />}</div>
           {buttonText}
