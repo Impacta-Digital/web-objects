cebf393a93d2f3c11837665d17fc5409cbbba534
miaslls <dev@camilasalles.dev>
2025-03-17 08:50:49 -0300

    feat(components): SQL `LabConclusion`

---
+++ src/pages/SQL/components/LabConclusion.tsx
@@ -0,0 +1,17 @@
+import { darkTheme } from '../../../constants/colors';
+
+export const LabConclusion = () => {
+  return (
+    <div className='wrapper'>
+      <p style={{ textAlign: 'center', fontSize: '1.4rem', marginBottom: 0 }}>
+        Parabéns! Você finalizou esta atividade.
+      </p>
+      <h1 style={{ color: darkTheme.accents.secondary, textTransform: 'uppercase', textAlign: 'center' }}>
+        Até a próxima!
+      </h1>
+
+      {/* - - - - - spacer */}
+      <div className='verticalSpacer' />
+    </div>
+  );
+};
