0d0e6926b99f0e87d4dc8e2bd847f4652a791079
miaslls <dev@camilasalles.dev>
2025-03-21 13:23:50 -0300

    fix: minor

--- src/pages/SQL/components/LabTitle.tsx
+++ src/pages/SQL/components/LabTitle.tsx
@@ -2,5 +2,5 @@ import { TitleAndDivider } from '../../../components/layout/TitleAndDivider';
 import { darkTheme } from '../../../constants/colors';
 
 export const LabTitle = (props: { title: string }) => {
-  return <TitleAndDivider {...props} subheading='Exercício de Laboratório' borderColor={darkTheme.accents.primary} />;
+  return <TitleAndDivider {...props} subheading='Exercícios de Laboratório' borderColor={darkTheme.accents.primary} />;
 };
